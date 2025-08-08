050b81e889a9bb8e2a81a5b7cdbdfdb6911bd513
WillDrac <willdrac@yahoo.com>
2025-08-08 09:49:31 -0400

    added top banner

--- inputs/infoBoxes.js
+++ inputs/infoBoxes.js
@@ -1,48 +1,48 @@
 const infoBoxes = [
     {
         title_en: "Testing item 1",
-        title_fr: "awok",
+        title_fr: "Boîte 1",
         image: "../images/ASMAC Logo.png",
         en: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-        fr: "uhesufihesuifhesuifhesh",
+        fr: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
         buttons: [
-            { en: "butn", fr: "boutin", link: "http://golflionhead.com/golf.htm" }
+            { en: "Register", fr: "Enregister", link: "http://golflionhead.com/golf.htm" }
         ]
     },
 
     {
         title_en: "Testing item 2",
-        title_fr: "jkok",
+        title_fr: "Boîte 2",
         image: "../images/AFASC Logo.png",
         en: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-        fr: "awdwadwadawddawdawdwadawdadwdawd",
+        fr: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
         buttons: [
-            { en: "buton", fr: "bwoutin", link: "http://golflionhead.com/golf.htm" },
-            { en: "buto2n", fr: "bwo2utin", link: "http://google.com" }
+            { en: "More Information", fr: "Plus d'informations", link: "http://golflionhead.com/golf.htm" },
+            { en: "Register", fr: "Enregister", link: "http://google.com" }
         ]
     },
 
     {
         title_en: "Testing item 3",
-        title_fr: "rok",
+        title_fr: "Boîte 3",
         en: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-        fr: "gdfdfgfdgdfgfdgd",
+        fr: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
         buttons: [
-            { en: "buton", fr: "bwoutin", link: "http://golflionhead.com/golf.htm" },
-            { en: "buto2n", fr: "bwo2utin", link: "http://google.com" },
-            { en: "buto3n", fr: "bwo3utin", link: "http://google.com" },
+            { en: "More Information", fr: "Plus d'informations", link: "http://golflionhead.com/golf.htm" },
+            { en: "Register for First Session", fr: "Enregister pour la première session", link: "http://google.com" },
+            { en: "Register for Second Session", fr: "Enregister pour la deuxième session" }
         ]
     },
 
     {
-        title_en: "Testing item 3",
-        title_fr: "rok",
+        title_en: "Testing item 4",
+        title_fr: "Boîte 4",
         en: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-        fr: "gdfdfgfdgdfgfdgd",
+        fr: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
         buttons: [
-            { en: "buton", fr: "bwoutin", link: "http://golflionhead.com/golf.htm" },
-            { en: "buto2n", fr: "bwo2utin", link: "http://google.com" },
-            { en: "buto3n", fr: "bwo3utin", link: "http://google.com" },
+            { en: "More Information", fr: "Plus d'informations", link: "http://golflionhead.com/golf.htm" },
+            { en: "Register for First Session", fr: "Enregister pour la première session", link: "http://google.com" },
+            { en: "Register for Second Session", fr: "Enregister pour la deuxième session" }
         ]
     }
 ]
